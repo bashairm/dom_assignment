fb5601cf7225aaf475b3c39463369dcea2a6018a
bashairm <bashairm95@hotmail.com>
2019-02-18 15:39:07 +0300

    done

--- starter-code/js/main.js
+++ starter-code/js/main.js
@@ -1,39 +1,43 @@
 const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
-const now = new Date();
-const seconds = now.getSeconds();
-const mins = now.getMinutes();
-const hour = now.getHours();
+var seconds
+var now
+var mins 
+var hour
+
 function setDate() {
+   now = new Date();
 
-  document.getElementById("dclock").innerHTML = now.toLocaleTimeString();
-
+   seconds = now.getSeconds();
   const secondsDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-  const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+   mins = now.getMinutes();
+  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
   minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
-  const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+   hour = now.getHours();
+  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+  document.getElementById("dclock").innerHTML = now.toLocaleTimeString();
+  document.getElementById("dclock1").innerHTML = hour + ':' + mins + ':' + seconds;
 
 }
 
 setInterval(setDate, 1000);
-
 setDate();
-var x = 1;
-function change() {
-  if (x == 1) {
-    document.getElementById("dclock").style.display = "none";
-  document.getElementById("dclock1").innerHTML = hour + ':' + mins + ':' + seconds;
+
+var x=1;
+function change(){
+if(x==1){
+  document.getElementById("dclock").style.display = "none";
   document.getElementById("dclock1").style.display = "block";
-    x++;
-  }
-  else {
-    document.getElementById("dclock1").style.display = "none";
-    document.getElementById("dclock").style.display = "block";
-    x = 1
-  }
+x++
+}
+else{
+  document.getElementById("dclock1").style.display = "none";
+  document.getElementById("dclock").style.display = "block";
+  x=1
+}
 }
